199d74e50e3473200c90ce7fd9ef474a008c5d7d
beipym <realbeipym@gmail.com>
2023-03-09 12:21:33 +0330

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,42 @@
 module.exports = {
+   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'theme-blue-5':'#000C19',
+      'theme-gray-3':'#868686'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundImage: {
+        // 'application': "url('/img/hero-pattern.svg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  },
   content: [
     "./src/**/*.{html,ts}",
   ],
