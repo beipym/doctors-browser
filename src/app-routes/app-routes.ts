0c4fb719984235b1ecd434814a63c2d413e3fec7
beipym <realbeipym@gmail.com>
2023-03-09 13:01:02 +0330

    update routes file

--- src/app-routes/app-routes.ts
+++ src/app-routes/app-routes.ts
@@ -13,8 +13,11 @@ export const appRoutes: Routes = [
         path:'app',
         component:LayoutComponent,
         children:[
-            { path:'home'         , loadChildren : () => import('../app/modules/home/home.module').then( m => m.HomeModule ) },
-            { path:'doctors-list' , loadChildren : () => import('../app/modules/doctors-list/doctors-list.module').then( m => m.DoctorsListModule ) }
+            { path:'home'           , loadChildren : () => import('../app/modules/home/home.module').then( m => m.HomeModule ) },
+            { path:'doctors-list'   , loadChildren : () => import('../app/modules/doctors-list/doctors-list.module').then( m => m.DoctorsListModule ) },
+            { path:'follow-up'      , loadChildren : () => import('../app/modules/follow-up/follow-up.module').then( m => m.FollowUpModule ) },
+            { path:'user-profile'   , loadChildren : () => import('../app/modules/user-profile/user-profile.module').then( m => m.UserProfileModule ) },
+            { path:'doctor-profile' , loadChildren : () => import('../app/modules/doctor-profile/doctor-profile.module').then( m => m.DoctorProfileModule ) }
         ]
     },
    
