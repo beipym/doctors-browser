a590ea61850f58ac716ae8d8226a487335534b47
beipym <realbeipym@gmail.com>
2023-03-09 18:49:09 +0330

    add user profile component

--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserProfileComponent } from './user-profile.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const userProfileRoutes:Routes=[
+  {
+    path:'',
+    component:UserProfileComponent
+  }
+]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userProfileRoutes)
   ]
 })
 export class UserProfileModule { }
