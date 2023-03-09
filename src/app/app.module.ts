2e4a19052eb33342b46c9b7020c05d138d366694
beipym <realbeipym@gmail.com>
2023-03-09 12:50:02 +0330

    fix problem with layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from 'src/app-routes/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BottomBarLayoutModule } from './layout/bottom-bar-layout/bottom-bar-layout.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BottomBarLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
