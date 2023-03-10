e775adde959482e1e4c3faf47b7dbdb4ddb3f4a6
beipym <realbeipym@gmail.com>
2023-03-10 19:41:04 +0330

    add profile image component

---
+++ src/@dtheme/image-viewer/image-viewer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dtheme-image-viewer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.scss']
+})
+export class ImageViewerComponent {
+
+}
