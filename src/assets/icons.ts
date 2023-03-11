7540cd173a4c953297e1e11cb632bab0ebd030c6
beipym <realbeipym@gmail.com>
2023-03-11 07:11:32 +0330

    update icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,9 +1,10 @@
 import { faCommentAlt } from "@fortawesome/free-regular-svg-icons";
-import { faComment, faHome, faHomeAlt, faHomeLgAlt, faStethoscope, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faAnglesLeft, faComment, faHome, faHomeAlt, faHomeLgAlt, faStethoscope, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const icons = {
     'faHome'        : faHomeLgAlt ,
     'faStethoscope' : faStethoscope,
     'faComments'    : faCommentAlt,
-    'faUser'        : faUser
+    'faUser'        : faUser,
+    'faAngleLeft'   : faAngleLeft
 }
