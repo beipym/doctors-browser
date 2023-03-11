b716486a0a983d2c62692faeec5e4f07fe7ba22b
beipym <realbeipym@gmail.com>
2023-03-11 07:09:34 +0330

    add accordion component

---
+++ src/@dtheme/details-accordion/details-accordion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dtheme-details-accordion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details-accordion.component.html',
+  styleUrls: ['./details-accordion.component.scss']
+})
+export class DetailsAccordionComponent {
+
+}
